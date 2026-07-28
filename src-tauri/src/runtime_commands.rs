49cd5bf7b4da1326e91d4ca48bc44c5a12af05e5
Phillip Chin <ekkus93@gmail.com>
2026-07-28 13:02:04 -0700

    Format runtime warning command

--- src-tauri/src/runtime_commands.rs
+++ src-tauri/src/runtime_commands.rs
@@ -3,8 +3,6 @@ use tauri::State;
 use crate::app_state::DesktopAppState;
 
 #[tauri::command]
-pub fn get_runtime_warnings(
-    state: State<'_, DesktopAppState>,
-) -> Result<Vec<String>, String> {
+pub fn get_runtime_warnings(state: State<'_, DesktopAppState>) -> Result<Vec<String>, String> {
     state.runtime_warnings()
 }
